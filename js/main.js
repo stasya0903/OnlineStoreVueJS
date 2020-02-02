41b00303d724fde0bbb0e85c7cfd296fbad1fc0b
stasya0903 <stasya0903@mail.ru>
2020-02-02 15:56:15 -0500

    adjusted from functional to  OOP

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-let products = [
-    {id: 1, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
-    {id: 2, title: "Mango People T-shirt", price: "53", img: "img/featured1.jpg"},
-    {id: 3, title: "Mango People T-shirt", price: "54", img: "img/featured1.jpg"},
-    {id: 4, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
-    {id: 5, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
-    {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
-    {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
-    {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"}
+class Product {
+    constructor(productObj) {
+        this.id = productObj.id;
+        this.title = productObj.title;
+        this.price = productObj.price;
+        this.img = productObj.img;
+    }
 
-];
-const RENDER = (title, price, img) => {
-    return `<article><div class="featured_content">
-        <a href="singlePage.html"><img class="product_image" src=${img} alt="productphoto"></a>
+    render() {
+
+        return `<article><div class="featured_content">
+        <a href="singlePage.html"><img class="product_image" src=${this.img} alt="productphoto"></a>
         <div class="product_text">
-            <a href="singlePage.html" class="product_link">${title}</a>
-            <p class="price">$${price}</p>
+            <a href="singlePage.html" class="product_link">${this.title}</a>
+            <p class="price">$${this.price}</p>
         </div>
         <a href="singlePage.html" class="product_add">
             <img class="cart product__cart" src="img/card.svg" alt=""><img src="img/" alt="">
@@ -22,20 +20,57 @@ const RENDER = (title, price, img) => {
         </a>
 
     </div></article>`
-};
 
-const RENDER_PRODUCTS = (list) => {
+    }
+}
 
-    let LIST_OF_PRODUCTS = "";
+class Products {
+    constructor(block) {
+        this.block = document.getElementById(block);
+        this.data = [];
+        this.allproducts = [];
+        this.init();
+    }
 
-    list.forEach((el) => LIST_OF_PRODUCTS += RENDER(el.title, el.price, el.img));
+    init() {
+        this.__fetchData();
+        this.__render();
+    }
 
-    document.getElementById("product_list").innerHTML = LIST_OF_PRODUCTS;
+    __fetchData() {
+        this.data = [
+            {id: 1, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
+            {id: 2, title: "Mango People T-shirt", price: "53", img: "img/featured1.jpg"},
+            {id: 3, title: "Mango People T-shirt", price: "54", img: "img/featured1.jpg"},
+            {id: 4, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
+            {id: 5, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
+            {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
+            {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"},
+            {id: 6, title: "Mango People T-shirt", price: "52", img: "img/featured1.jpg"}
 
-};
+        ];
+    }
+
+    __render() {
+        this.data.forEach((el) => {
+            let product = new Product(el);
+            this.allproducts.push(product);
+            this.block.insertAdjacentHTML("afterbegin",product.render());
+
+        })
+
+    }
+}
 
 
-RENDER_PRODUCTS(products);
+class Basket {
+    constructor(items, total) {
+
+    }
+}
+ new Products("product_list");
+
+
 
 
 
